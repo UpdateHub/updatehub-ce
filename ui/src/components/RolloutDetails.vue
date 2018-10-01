14d42a16a23f99bfb6ab371016c06c8373da5527
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2018-10-01 10:02:00 -0300

    ui: updated RolloutDetails

--- ui/src/components/RolloutDetails.vue
+++ ui/src/components/RolloutDetails.vue
@@ -8,6 +8,13 @@
       <router-link to="/rollouts" class="btn btn-link float-sm-right"><i class="fas fa-caret-left"></i> Back to Rollout List</router-link>
     </h3>
   </div>
+  <div class="alert d-flex flex-row" v-if="hasSuccessfullyFinished() || hasError()" v-bind:class="{ 'alert-danger': hasError(), 'alert-success': hasSuccessfullyFinished()  }" role="alert">
+    <i class="fas fa-4x" v-bind:class="{ 'fa-check-circle': hasSuccessfullyFinished(), 'fa-exclamation-circle': hasError() }"></i>
+    <div class="align-self-center ml-2">
+      <span if="hasSuccessfullyFinished()"> All of <strong>{{ rollout.devices.length }}</strong> devices has been updated successfully!</span>
+      <span if="hasError()"> <strong>{{ rollout.statistics.failed }}</strong> of <strong>{{ rollout.devices.length }}</strong> devices has been failed while updating!</span>
+    </div>
+  </div>
   <div class="card-group" v-if="rollout.package.uid">
     <div class="card card-body">
       <ul class="list-group list-group-flush">
@@ -62,17 +69,44 @@
       </ul>
     </div>
   </div>
+
+ <table class="table table-hover mt-4">
+        <thead class="thead-light">
+          <tr><th>UID</th><th>Version</th><th>Hardware</th><th>Status</th><th></th></tr>
+        </thead>
+        <tbody>
+            <template v-for="device in rollout.devices">
+            <tr :key="device.uid" @click="toggleOpened(device.uid)" v-bind:class="deviceRowContextualClass(device)">
+              <td><span>{{ device.uid }}</span></td>
+              <td><span>{{ device.version }}</span></td>
+              <td><span>{{ device.hardware }}</span></td>
+              <td><span class="text-capitalize">{{ device.status }}</span></td>
+              <td><i class="fas" v-bind:class="{ 'fa-caret-up': opened == device.uid, 'fa-caret-down': opened != device.uid }"></i></td>
+            </tr>
+            <tr :key="device.uid + 'details'" v-if="opened == device.uid">
+              <td colspan="5">
+                <DeviceDetails :uid="device.uid" embedded="true"></DeviceDetails>
+              </td>
+            </tr>
+            </template>
+        </tbody>
+      </table>
 </div>
 </template>
 
 <script>
+import DeviceDetails from "./DeviceDetails";
+
 export default {
   name: "RolloutDetails",
+
+  components: { DeviceDetails },
 
   data() {
     return {
       rollout: { package: {}, devices: [], statistics: {}, status: "" },
-      timer: null
+      timer: null,
+      opened: "",
     };
   },
 
@@ -92,13 +126,24 @@ export default {
         rollout.package = await this.getPackage(rollout.package);
         rollout.statistics = await this.getStatistics(rollout);
 
-        if (rollout.finished_at > rollout.started_at) {
-          rollout.status = "finished";
-        } else if (rollout.running) {
+        if (rollout.running) {
           rollout.status = "running";
         } else {
-          rollout.status = "paused";
+          if (rollout.finished_at > rollout.started_at) {
+            if (this.rollout.statistics.updated == this.rollout.devices.length) {
+              rollout.status = "finished";
+            } else {
+              rollout.status = "failed"
+            }
+          } else {
+            rollout.status = "paused";
+          }
         }
+
+        rollout.devices = await Promise.all(rollout.devices.map(async d => {
+            let device = await this.getDevice(d)
+            return device
+        }));
 
         return rollout;
       });
@@ -124,6 +169,43 @@ export default {
         .then(res => {
           return res.data;
         });
+    },
+
+    async getDevice(uid) {
+      return await this.$http
+        .get("/api/devices/" + uid)
+        .then(res => {
+          return res.data;
+      })
+    },
+
+    toggleOpened(uid) {
+      if (this.opened == uid) {
+        this.opened = "";
+      } else {
+        this.opened = "";
+        this.$nextTick(() => {
+          this.opened = uid;
+        });
+      }
+    },
+
+    hasError() {
+      return this.rollout.status == "failed" &&
+        this.rollout.statistics.failed > 0;
+    },
+
+    hasSuccessfullyFinished() {
+      return this.rollout.status == "finished" &&
+        this.rollout.statistics.updated == this.rollout.devices.length;
+    },
+
+    deviceRowContextualClass(device) {
+      return {
+        "updated": "table-success",
+        "failed": "table-danger",
+        "pending": "table-secondary"
+      }[device.status]
     }
   },
 
